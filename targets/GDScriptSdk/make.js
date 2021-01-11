3a6e8b101460066babb09e952667cd5ad47fd7cb
MizunagiKB <mizukb@LIVE.JP>
2021-01-11 17:21:29 +0900

    Changed HTTPRequest to HTTPClient and queued API calls.

--- targets/GDScriptSdk/make.js
+++ targets/GDScriptSdk/make.js
@@ -13,102 +13,43 @@ function getVerticalNameDefault() {
 }
 
 
-function getAuthParams(apiCall) {
-    if (apiCall.url === "/Authentication/GetEntityToken")
-        return "authKey, authValue";
-    if (apiCall.auth === "EntityToken")
-        return "\"X-EntityToken\", PlayFabSettings._internalSettings.EntityToken";
-    if (apiCall.auth === "SecretKey")
-        return "\"X-SecretKey\", PlayFabSettings.DeveloperSecretKey";
-    else if (apiCall.auth === "SessionTicket")
-        return "\"X-Authorization\", PlayFabSettings._internalSettings.ClientSessionTicket";
-    return "null, null";
-}
-
-
 function getRequestActions(tabbing, apiCall) {
     if (apiCall.result === "LoginResult" || apiCall.request === "RegisterPlayFabUserRequest") {
-        str_result = "";
-        str_result += tabbing + "if \"TitleId\" in dict_request:\n";
-        str_result += tabbing + "    pass\n";
-        str_result += tabbing + "else:\n";
-        str_result += tabbing + "    dict_request[\"TitleId\"] = PlayFabSettings.TitleId\n";
-        str_result += tabbing + "if not dict_request[\"TitleId\"]:\n";
-        str_result += tabbing + "    # Must be have TitleId set to call this method\n";
-        str_result += tabbing + "    assert(false)\n";
-        return str_result;
+        return "[PlayFab.E_PRO.USE_TITLE_ID]";
     }
     if (apiCall.auth === "EntityToken") {
-        str_result = "";
-        str_result += tabbing + "if not PlayFabSettings._internalSettings.EntityToken:\n";
-        str_result += tabbing + "    # Must call GetEntityToken before calling this method\n";
-        str_result += tabbing + "    assert(false)\n";
-        return str_result;
+        return "[PlayFab.E_PRO.CHK_ENTITY_TOKEN, PlayFab.E_PRO.USE_AUTH]";
     }
     if (apiCall.auth === "SessionTicket") {
-        str_result = "";
-        str_result += tabbing + "if not PlayFabSettings._internalSettings.ClientSessionTicket:\n";
-        str_result += tabbing + "    # Must be logged in to call this method\n";
-        str_result += tabbing + "    assert(false)\n";
-        return str_result;
+        return "[PlayFab.E_PRO.CHK_SESSION_TICKET, PlayFab.E_PRO.USE_AUTH]";
     }
     if (apiCall.auth === "SecretKey") {
-        str_result = "";
-        str_result += tabbing + "if not PlayFabSettings._internalSettings.DeveloperSecretKey:\n";
-        str_result += tabbing + "    # Must have DeveloperSecretKey set to call this method\n";
-        str_result += tabbing + "    assert(false)\n";
-        return str_result;
+        return "[PlayFab.E_PRO.CHK_SECRET_KEY, PlayFab.E_PRO.USE_AUTH]";
     }
-    if (apiCall.url === "/Authentication/GetEntityToken")
-        return tabbing + "var authKey = null\n"
-            + tabbing + "var authValue = null\n\n"
-            + tabbing + "if PlayFabSettings._internalSettings.EntityToken:\n"
-            + tabbing + "    authKey = \"X-EntityToken\"\n"
-            + tabbing + "    authValue = PlayFabSettings._internalSettings.EntityToken\n"
-            + tabbing + "elif PlayFabSettings._internalSettings.ClientSessionTicket:\n"
-            + tabbing + "    authKey = \"X-Authorization\"\n"
-            + tabbing + "    authValue = PlayFabSettings._internalSettings.ClientSessionTicket \n"
-            + tabbing + "elif PlayFabSettings.DeveloperSecretKey:\n"
-            + tabbing + "    authKey = \"X-SecretKey\"\n"
-            + tabbing + "    authValue = PlayFabSettings.DeveloperSecretKey \n";
-    return "";
+    if (apiCall.url === "/Authentication/GetEntityToken") {
+        return "[PlayFab.E_PRO.USE_AUTH]"
+    }
+
+    return "[]";
 }
 
 
 function getResultActions(tabbing, apiCall, api) {
 
     if (apiCall.result === "LoginResult") {
-        str_result = "";
-        str_result += tabbing + "PlayFabHTTPRequest.reset_connection()\n"
-        str_result += tabbing + "PlayFabHTTPRequest.connect(\"request_completed\", PlayFabHTTPRequest, \"_evt_LoginResult\")"
-        return str_result;
+        return "[PlayFab.E_EPI.UPD_SESSION_TICKET, PlayFab.E_EPI.UPD_ENTITY_TOKEN, PlayFab.E_EPI.REQ_MULTI_STEP_CLIENT_LOGIN]";
     }
     else if (apiCall.result === "RegisterPlayFabUserResult") {
-        str_result = "";
-        str_result += tabbing + "PlayFabHTTPRequest.reset_connection()\n"
-        str_result += tabbing + "PlayFabHTTPRequest.connect(\"request_completed\", PlayFabHTTPRequest, \"_evt_RegisterPlayFabUserResult\")"
-        return str_result;
+        return "[PlayFab.E_EPI.UPD_SESSION_TICKET, PlayFab.E_EPI.REQ_MULTI_STEP_CLIENT_LOGIN]";
     }
     else if (apiCall.result === "AttributeInstallResult") {
-        str_result = "";
-        str_result += tabbing + "PlayFabHTTPRequest.reset_connection()\n"
-        str_result += tabbing + "PlayFabHTTPRequest.connect(\"request_completed\", PlayFabHTTPRequest, \"_evt_AttributeInstallResult\")"
-        return str_result;
+        return "[PlayFab.E_EPI.UPD_ATTRIBUTE]";
     }
     else if (apiCall.result === "GetEntityTokenResponse") {
-        str_result = "";
-        str_result += tabbing + "PlayFabHTTPRequest.reset_connection()\n"
-        str_result += tabbing + "PlayFabHTTPRequest.connect(\"request_completed\", PlayFabHTTPRequest, \"_evt_GetEntityTokenResponse\")"
-        return str_result;
-    }
-    else {
-        str_result = "";
-        str_result += tabbing + "PlayFabHTTPRequest.reset_connection()\n"
-        str_result += tabbing + "PlayFabHTTPRequest.connect(\"request_completed\", PlayFabHTTPRequest, \"_evt_RequestCompleted\")"
-        return str_result;
+        return "[PlayFab.E_EPI.UPD_ENTITY_TOKEN]";
     }
 
-    return "";
+    return "[]";
 }
 
 
@@ -132,14 +73,13 @@ exports.makeCombinedAPI = function (apis, sourceDir, apiOutputDir) {
 
             var api_locals = {
                 api: apis[i],
-                getAuthParams: getAuthParams,
                 getRequestActions: getRequestActions,
                 getResultActions: getResultActions,
                 hasClientOptions: getAuthMechanisms([apis[i]]).includes("SessionTicket")
             }
 
             var apiTemplate = getCompiledTemplate(path.resolve(sourceDir, "templates/API.gd.ejs"));
-            writeFile(path.resolve(apiOutputDir, "PlayFabSDK/PlayFab" + apis[i].name + "API.gd"), apiTemplate(api_locals));
+            writeFile(path.resolve(apiOutputDir, "PlayFabSDK/PlayFabAPI_" + apis[i].name + ".gd"), apiTemplate(api_locals));
         }
     }
 
